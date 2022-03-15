ad982ed379cc017622e9ce2a7f32aa9025cf4aef
Medupi <medupi@nedscaper.com>
2022-03-16 01:48:33 +0200

    Created React App

---
+++ reach-insurance/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>REACH INSURANCE</h1>
+        <br/>
+        <p>
+          gain eternity...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
