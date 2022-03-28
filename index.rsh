09bea7d7113b965d6a4261ca88d590e792aa854e
Eng. Rodgers Ategyeka <atrodgers77@gmail.com>
2022-03-28 23:22:50 +0300

    designed the login and register pages in react

--- index.rsh
+++ index.rsh
@@ -1,79 +1,78 @@
 'reach 0.1';
 
-
 export const main = Reach.App(() => {
     const Insurer = Participant('Insurer', {
         communityGroupName: Bytes,
-        createDatabase: Fun([], Null ),
+        createDatabase: Fun([], Null),
         approveNewMembership: Fun([Address], Null),
         createAddressForNewUSer: Fun([], Address),
         createInvoices: Fun([], Null),
-        moveMaturedPayments: Fun([], Null ),
+        moveMaturedPayments: Fun([], Null),
     });
     const CommunityMember = ParticipantClass('CommunityMember', {
-        registerMembership: Fun([{name: Bytes, email: Bytes}], Bool ),
-        registerDependant: Fun([Address], Bool ),
-        cancelMembership: Fun([Address], Bool ),
-        payEntryFee: Fun([Address, UInt], Bool ),
-        payMonthlyFee: Fun([Address, UInt], Bool ),
-        changePackage: Fun([Bytes], Bool ),
-        getAccountStatement: Fun([Address], Map ),
-        createClaim: Fun([Address, UInt], Bool ),
-        respondToClaim: Fun([Address, Bool], datatypexxxxx ),
-        inheritAccount: Fun([], datatypexxxxx ),
-        approveInheritance: Fun([], datatypexxxxx ),
-        approveExit: Fun([], datatypexxxxx ),
+        registerMembership: Fun([{ name: Bytes, email: Bytes }], Bool),
+        registerDependant: Fun([Address], Bool),
+        cancelMembership: Fun([Address], Bool),
+        payEntryFee: Fun([Address, UInt], Bool),
+        payMonthlyFee: Fun([Address, UInt], Bool),
+        changePackage: Fun([Bytes], Bool),
+        getAccountStatement: Fun([Address], Map),
+        createClaim: Fun([Address, UInt], Bool),
+        respondToClaim: Fun([Address, Bool], Null),
+        inheritAccount: Fun([], Null),
+        approveInheritance: Fun([], Null),
+        approveExit: Fun([], Null),
     });
     const Dependant = ParticipantClass('Dependant', {
-        getAccountStatement: Fun([], datatypexxxxx ),
-        payMonthlyFee: Fun([], datatypexxxxx ),
-        createClaim: Fun([], datatypexxxxx ),
-        inheritAccount: Fun([], datatypexxxxx ),
-        approveInheritance: Fun([], datatypexxxxx ),
+        getAccountStatement: Fun([], Null),
+        payMonthlyFee: Fun([], Null),
+        createClaim: Fun([], Null),
+        inheritAccount: Fun([], Null),
+        approveInheritance: Fun([], Null),
     });
     const ImSvcProvider = ParticipantClass('ImSvcProvider', {
-        registerMembership: Fun([], datatypexxxxx ),
-        cancelMembership: Fun([], datatypexxxxx ),
-        payEntryFee: Fun([], datatypexxxxx ),
-        createClaim: Fun([], datatypexxxxx ),
-        respondToClaim: Fun([], datatypexxxxx ),
+        registerMembership: Fun([], Null),
+        cancelMembership: Fun([], Null),
+        payEntryFee: Fun([], Null),
+        createClaim: Fun([], Null),
+        respondToClaim: Fun([], Null),
     });
     init();
-    
+
     Insurer.only(() => {
-        const {nftId, minBid, lenInBlocks} = declassify(interact.getSale());
+        const { nftId, minBid, lenInBlocks } = declassify(interact.getSale());
     });
     Insurer.publish(nftId, minBid, lenInBlocks);
     const amt = 1;
     commit();
-    
+
     Insurer.pay([[amt, nftId]]);
     Insurer.interact.auctionReady();
-    
+
     assert(balance(nftId) == amt, "balance of NFT is wrong");
     const end = lastConsensusTime() + lenInBlocks;
-    
-    
-    fork().case( CommunityMember, (() => ({
-              
-                 msg: 19,
-                 when: declassify(interact.keepGoing()) 
-              })),
-              ((v) => v),
-              (v) => {
-                require(v == 19);
-                transfer(wager + 19).to(this);
-                commit();
-                exit();
-              }
-              
-         ).case( Dependant, (() => ({
-          
-             when: declassify(interact.keepGoing())
-             _local: interact.secretVal 
-          })),
-          ((_) => wager),
-          (_, _secV) => {
+
+
+    fork().case(CommunityMember, (() => ({
+
+        msg: 19,
+        when: declassify(interact.keepGoing())
+    })),
+        ((v) => v),
+        (v) => {
+            require(v == 19);
+            transfer(wager + 19).to(this);
+            commit();
+            exit();
+        }
+
+    ).case(Dependant, (() => ({
+
+        when: declassify(interact.keepGoing()),
+        _local: interact.secretVal
+    })),
+        ((_) => wager),
+        (_, _secV) => {
             commit();
             Bob.only(() => interact.showSecret(_secV))
             Alice.only(() => interact.showOpponent(Bob));
@@ -81,15 +80,15 @@ export const main = Reach.App(() => {
             transfer(2 * wager).to(this);
             commit();
             exit();
-          }
-          
-         ).case( ImSvcProvider, (() => ({
-          
-             when: declassify(interact.keepGoing())
-             _local: interact.secretVal 
-          })),
-          ((_) => wager),
-          (_, _secV) => {
+        }
+
+    ).case(ImSvcProvider, (() => ({
+
+        when: declassify(interact.keepGoing()),
+        _local: interact.secretVal
+    })),
+        ((_) => wager),
+        (_, _secV) => {
             commit();
             Bob.only(() => interact.showSecret(_secV))
             Alice.only(() => interact.showOpponent(Bob));
@@ -97,19 +96,19 @@ export const main = Reach.App(() => {
             transfer(2 * wager).to(this);
             commit();
             exit();
-          }
-          
-         ).timeout(deadline, () => {
-             
-             race( ...CommunityMember ).publish();
-             race( ...Dependant ).publish();
-             race( ...ImSvcProvider ).publish();
-             
-             commit();
-             exit(); 
-             
-          });
-          
+        }
+
+    ).timeout(deadline, () => {
+
+        race(...CommunityMember).publish();
+        race(...Dependant).publish();
+        race(...ImSvcProvider).publish();
+
+        commit();
+        exit();
+
+    });
+
     commit();
     exit();
 });
--- src/App.js
+++ src/App.js
@@ -1,18 +1,149 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import * as backend from './reach-build/index.main.mjs';
+import { loadStdlib } from '@reach-sh/stdlib';
+const reach = loadStdlib(process.env);
 
 function App() {
+  const mnemonicRef = useRef(<></>);
+  const [addressStr, setAddressStr] = useState("");
+  const [mnemonicStr, setMnemonicStr] = useState("");
+  const [gotoSignup, setGotoSignup] = useState(false);
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [insrPackage, setInsrPackage] = useState("");
+
+
+  function Login() {
+    //alert(mnemonicStr);
+    setGotoSignup(true);
+  }
+
+  function newAlgoAccount() {
+    alert("NOTE: a new algo account is being created for you, please dont forget to copy your new mnemonic and keep it secret.");
+    setMnemonicStr("the mnemonic phrase for the newly created account");
+  }
+
+  function Signup() {
+    //alert("please wait ...");
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>REACH INSURANCE</h1>
-        <br/>
-        <p>
-          Gain Eternity...
-        </p>
-      </header>
-    </div>
+    <>
+      {(!gotoSignup) ?
+        <div className='h-screen flex bg-blue-100'>
+          <div className='w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-2 px-16 shadow'>
+            <h1 className='text-4xl text-blue-700  text-primary mt-2 mb-2 text-center'> Insurance Dapp </h1>
+            <hr />
+
+            <h1 className='text-xl font-medium text-primary mt-6 mb-6 text-center'>
+              Enter Your mnemonic to Login
+            </h1>
+
+            <form >
+              <div>
+                <input ref={mnemonicRef}
+                  type="text"
+                  value={mnemonicStr}
+                  onChange={e => setMnemonicStr(e.target.value)}
+                  className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                  id='mnemonic'
+                  placeholder='12-word phrase'
+                />
+              </div>
+
+              <div>
+                <label>Have no Algorand account ? <span onClick={newAlgoAccount} href='#' className='text-blue-600'>Create a new</span> </label>
+
+              </div>
+
+              <div className='flex justify-center items-center mt-6'>
+                <button onClick={Login}
+                  className={` bg-blue-500 py-2 px-4 text-sm text-white rounded border border-green focus:outline-none focus:border-greenn-dark`}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        :
+        <div className='h-screen flex bg-blue-100'>
+          <div className='w-full max-w-md m-auto bg-white rounded-lg border border-primaryBorder shadow-default py-2 px-16 shadow'>
+            <h1 className='text-4xl text-blue-700  text-primary mt-2 mb-2 text-center'> Insurance Dapp </h1>
+            <hr />
+            <small> Keep your mnemonic secret: ( {mnemonicStr} ) </small>
+            <hr />
+
+            <h1 className='text-xl font-medium text-primary mt-6 mb-6 text-center'>
+              Register for insurance services
+            </h1>
+
+            <form >
+              <div>
+                <label htmlFor='fullname'>Full name </label>
+                <input
+                  type="text"
+                  value={fullname}
+                  onChange={e => setFullname(e.target.value)}
+                  className={`w-full py px-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                  id='fullname'
+                />
+              </div>
+
+              <div>
+                <label htmlFor='phone'>Phone number </label>
+                <input
+                  type="text"
+                  value={phone}
+                  onChange={e => setPhone(e.target.value)}
+                  className={`w-full py px-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                  id='phone'
+                />
+              </div>
+
+              <div>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  className={`w-full py px-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                  placeholder='your@email.com'
+                />
+              </div>
+
+              <div>
+                <label htmlFor='phone'> Select the insurance package you prefer </label>
+                <select
+                  type="select"
+                  value={insrPackage}
+                  onChange={e => setInsrPackage(e.target.value)}
+                  className={`w-full py px-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+                >
+                  <option> package1 - 1000 - 120,000 </option>
+                  <option> package2 - 5000 - 600,000 </option>
+                  <option> package3 - 10,000 - 1,200,000 </option>
+                  <option> package4 - 50,000 - 6,000,000 </option>
+                  <option> package5 - 100,000 - 12,000,000 </option>
+                  <option> package6 - 500,000 - 60,000,000 </option>
+                </select>
+              </div>
+
+
+              <div className='flex justify-center items-center mt-6'>
+                <button onClick={Signup}
+                  className={` bg-blue-500 py-2 px-4 text-sm text-white rounded border border-green focus:outline-none focus:border-greenn-dark`}
+                >
+                  Register now
+                </button>
+              </div>
+            </form>
+          </div>
+        </div >
+      }
+    </>
   );
 }
 
